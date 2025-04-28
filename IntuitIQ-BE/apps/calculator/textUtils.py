2b228f46e56c972db2d51e2d31a5e8d14a4c928e
HimeshBhattacharjee <omandabamema.2130@gmail.com>
2025-04-28 11:08:39 +0530

    Debugging

--- IntuitIQ-BE/apps/calculator/textUtils.py
+++ IntuitIQ-BE/apps/calculator/textUtils.py
@@ -1,13 +1,11 @@
-from mistralai.client import MistralClient
+from mistralai import Mistral
 import markdown
 from constants import MISTRAL_API_KEY
 
 if not MISTRAL_API_KEY:
     raise ValueError("MISTRAL_API_KEY not found in environment variables.")
 
-print(MISTRAL_API_KEY)
-
-client = MistralClient(api_key=MISTRAL_API_KEY)
+client = Mistral(api_key=MISTRAL_API_KEY)
 model = "mistral-large-latest"
 
 def analyze_text(question: str):
